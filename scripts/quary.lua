eb0a181fc285129d427c0d9c4c20dde8623c8101
Tyler Helmuth <tyler.helmuth@bplglobal.net>
2020-04-15 15:25:38 -0400

    good stuff

--- scripts/quary.lua
+++ scripts/quary.lua
@@ -10,19 +10,20 @@ os.loadAPI("mining")
 
 LOG_FILE = fs.open("log", "w")
 
+args = {...}
+
 function Log(text)
     LOG_FILE.writeLine(text)
 end
 
 function Spiral(length, height)
-    if length == 2 then
+    if length == 1 then
         return
     end
 
     --- move length - 1 blocks right, up, left, length - 2 down and then recurse! :)
 
     -- RIGHT
-    turtle.turnRight()
     DigLine(length - 1, height)
 
     -- UP
@@ -37,13 +38,20 @@ function Spiral(length, height)
     turtle.turnLeft()
     DigLine(length - 2, height)
 
+    turtle.turnLeft()
     Spiral(length - 1, height)
 end
 
 function DigLine(length, height)
+    local up = true
     for i = 1,length do
-        local up = (i - 1 % 0) -- first dig goes up, second comes down
+        up = ((i - 1) % 2 == 0) -- first dig goes up, second comes down
         DigSpot(height, up)
+    end
+
+    --we need to come down
+    if up then
+        mining.MoveDown(height - 1)
     end
 end
 
@@ -52,20 +60,18 @@ function DigSpot(height, up)
     mining.MoveForward()
 
     local moveFn = up and mining.MoveUp or mining.MoveDown
-    for i = 1, height - 1 do
-        moveFn()
-    end
+    moveFn(height - 1)
 end
 
 function Run()
-    args = {}
     local length = args[1]
     local height = args[2]
 
     Log("Kicking off quary with length = " .. length .. " and height = " .. height)
+    turtle.turnRight()
     Spiral(length, height)
 end
 
-pcall(Run)
+Run()
 
 LOG_FILE.close()
