aeebefa5cf669095094fd9f651d8efc4dd161558
Tyler Helmuth <tyler.helmuth@bplglobal.net>
2020-04-17 13:48:34 -0400

    hjkhjk

--- turtlecp.sh
+++ turtlecp.sh
@@ -5,10 +5,11 @@ git pull
 
 cd ~/minecraft-server
 
-for filename in /scripts do
-    basename = $(echo "$filename" | cut -f 1 -d '.')
+for filename in /scripts; do
+    echo $filename
+    basename=$(echo "$filename" | cut -f 1 -d '.')
 
-    for turtle in $MINECRAFT_DATA/computer/*/ do
+    for turtle in $MINECRAFT_DATA/computer/*/; do
         path = "${MINECRAFT_DATA}/computer/${turtle}/${basename}"
         cp $basename $path
         chmod 777 $path
