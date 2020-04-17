b4199ea7d3dd42e604331770aec4795c67d85ac8
Tyler Helmuth <tyler.helmuth@bplglobal.net>
2020-04-17 14:01:01 -0400

    asdf

--- turtlecp.sh
+++ turtlecp.sh
@@ -8,9 +8,10 @@ cd ~/minecraft-server
 for filename in /scripts; do
     echo $filename
     basename=$(echo "$filename" | cut -f 1 -d '.')
-
+    echo "basename: ${basename}"
     for turtle in $MINECRAFT_DATA/computer/*/; do
         path = "${MINECRAFT_DATA}/computer/${turtle}/${basename}"
+        echo "path: ${path}"
         cp $basename $path
         chmod 777 $path
     done
