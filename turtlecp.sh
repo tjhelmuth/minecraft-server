3c59409e568e41f80725b428a8b5840b49875278
Tyler Helmuth <tyler.helmuth@bplglobal.net>
2020-04-20 21:58:25 -0400

    skyblock precious

--- turtlecp.sh
+++ turtlecp.sh
@@ -10,7 +10,7 @@ for filename in scripts/*; do
     basename=$(echo "$filename" | cut -f 1 -d '.')
     basename=$(basename "$basename")
     echo "basename: ${basename}"
-    for turtle in $MINECRAFT_DATA/computer/*/; do
+    for turtle in ${MINECRAFT_DATA}computer/*/; do
         path="${turtle}${basename}"
         echo "path: ${path}"
         cp $filename $path
